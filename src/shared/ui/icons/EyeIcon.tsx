5af96c079bab1b2507e6bd7268f8646caca00954
Aeksandr M. <alfie.m1r@yandex.ru>
2026-03-01 12:50:48 +0300

    Правки svg атрибутов для react

--- src/shared/ui/icons/EyeIcon.tsx
+++ src/shared/ui/icons/EyeIcon.tsx
@@ -55,9 +55,9 @@ const EyeOffIcon: FC = () => {
                     width="21.6883"
                     height="24"
                     filterUnits="userSpaceOnUse"
-                    color-interpolation-filters="sRGB"
+                    colorInterpolationFilters="sRGB"
                 >
-                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
                     <feBlend
                         mode="normal"
                         in="SourceGraphic"
